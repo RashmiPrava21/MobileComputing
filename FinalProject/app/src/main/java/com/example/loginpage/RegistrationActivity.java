9feff006741d2717a323f1b6bbfc6dfe35a5893c
Daniel Volinski <dan52@umbc.edu>
2019-04-17 15:58:09 -0400

    Removed top bar from registration page.

--- FinalProject/app/src/main/java/com/example/loginpage/RegistrationActivity.java
+++ FinalProject/app/src/main/java/com/example/loginpage/RegistrationActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,7 +29,17 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+
+        }
+        catch (NullPointerException e){}
+
+        setContentView(R.layout.activity_main);
 
         setupUI();
 
