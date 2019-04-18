d6fac57e507ff94daee1b4e86a196b0ca8d3b260
Daniel Volinski <dan52@umbc.edu>
2019-04-18 12:35:47 -0400

    Pushing new HomePage.

---
+++ FinalProject/app/src/main/java/com/example/loginpage/BarChartActivity.java
@@ -0,0 +1,85 @@
+package com.example.loginpage;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.XAxis.XAxisPosition;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+// import com.github.mikephil.charting.formatter.XAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.model.GradientColor;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarChartActivity extends AppCompatActivity {
+
+    BarChart barChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barchart);
+
+        barChart = (BarChart) findViewById(R.id.barchart);
+
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(true);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+        barEntries.add(new BarEntry(1, (int) 40f));
+        barEntries.add(new BarEntry(1, (int) 40f));
+        barEntries.add(new BarEntry(1, (int) 40f));
+        barEntries.add(new BarEntry(1, (int) 40f));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Data Set1");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData((IBarDataSet) barDataSet);
+        data.setBarWidth(0.9f);
+
+        barChart.setData(data);
+
+        String[] days = new String[]{"Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"};
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter((ValueFormatter) new MyXAxisValueFormatter(days));
+        xAxis.setPosition(XAxisPosition.BOTH_SIDED);
+        xAxis.setGranularity(1);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setAxisMinimum(1);
+
+    }
+
+    public class MyXAxisValueFormatter extends ValueFormatter {
+
+        private String[] mValues;
+        public MyXAxisValueFormatter(String[] values){
+            this.mValues = values;
+        }
+
+        public String getFormattedValue(float value) {
+            return mValues[(int)value];
+
+        }
+
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            return null;
+        }
+    }
+}
