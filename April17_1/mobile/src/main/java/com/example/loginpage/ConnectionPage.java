310d1b36c6a40197bade290650625d06eff2f2d2
Daniel Volinski <dan52@umbc.edu>
2019-05-01 15:37:35 -0400

    Updated notification for mobile and wear connection.

--- April17_1/mobile/src/main/java/com/example/loginpage/ConnectionPage.java
+++ April17_1/mobile/src/main/java/com/example/loginpage/ConnectionPage.java
@@ -1,11 +1,29 @@
 package com.example.loginpage;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class ConnectionPage extends AppCompatActivity {
+
+    TextView textview;
+    protected Handler myHandler;
+    String bundleMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +39,85 @@ public class ConnectionPage extends AppCompatActivity {
         catch (NullPointerException e){}
 
         setContentView(R.layout.activity_connection_page);
+
+        textview = findViewById(R.id.connectingToWatch);
+
+        Bundle b = getIntent().getExtras();
+        bundleMessage = (String) b.getSerializable("Retrieve message!!");
+        Log.d("Received", "Received String!!" + bundleMessage);
+
+        String message = "Sending message.... ";
+        textview.setText(message);
+
+        //Sending a message can block the main UI thread, so use a new thread//
+
+        new ConnectionPage.NewThread("/my_path", message).start();
+
+
+        myHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Bundle stuff = msg.getData();
+                messageText(stuff.getString("messageText"));
+                return true;
+            }
+        });
+    }
+
+    public void messageText(String newinfo) {
+        if (newinfo.compareTo("") != 0) {
+            textview.append("\n" + newinfo);
+        }
+    }
+
+
+    //Use a Bundle to encapsulate our message//
+    public void sendmessage(String messageText) {
+        Bundle bundle = new Bundle();
+        bundle.putString("messageText", messageText);
+        Message msg = myHandler.obtainMessage();
+        msg.setData(bundle);
+        myHandler.sendMessage(msg);
+
+    }
+
+    class NewThread extends Thread {
+        String path;
+        String message;
+
+        //Constructor for sending information to the Data Layer//
+
+        NewThread(String p, String m) {
+            path = p;
+            message = m;
+        }
+
+        public void run() {
+            //Retrieve the connected devices, known as nodes//
+            Task<List<Node>> wearableList = Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+
+            try {
+                List<Node> nodes = Tasks.await(wearableList);
+                for (Node node : nodes) {
+                    Task<Integer> sendMessageTask =
+                            //Send the message//
+                            Wearable.getMessageClient(ConnectionPage.this).sendMessage(node.getId(), path, message.getBytes());
+                    try {
+                        //Block on a task and get the result synchronously//
+                        Integer result = Tasks.await(sendMessageTask);
+                        sendmessage("Tryint to Connect!!");
+                        //if the Task fails, then…..//
+                    } catch (ExecutionException exception) {
+                        //TO DO: Handle the exception//
+                    } catch (InterruptedException exception) {
+                        //TO DO: Handle the exception//
+                    }
+                }
+            } catch (ExecutionException exception) {
+                //TO DO: Handle the exception//
+            } catch (InterruptedException exception) {
+                //TO DO: Handle the exception//
+            }
+        }
     }
 }
