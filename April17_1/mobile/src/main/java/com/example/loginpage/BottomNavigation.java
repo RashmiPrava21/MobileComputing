00274a0c654cc7f22a88192589f3f74826a2b516
saichandana14 <chandana.chinni14@gmail.com>
2019-05-06 15:40:44 -0400

    Changes in Profile page

---
+++ April17_1/mobile/src/main/java/com/example/loginpage/BottomNavigation.java
@@ -0,0 +1,45 @@
+package com.example.loginpage;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class BottomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+        BottomNavigationView bottonNav = findViewById(R.id.bottom_n);
+        bottonNav.setOnNavigationItemReselectedListener(navListener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener navListener =
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_sleep:
+                            selectedFragment = new HomePage();
+                            break;
+                        case R.id.nav_dashboard:
+                            selectedFragment = new DashboardPage();
+                            break;
+                        case R.id.nav_account:
+                            selectedFragment = new AccountPage();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    
+                    }
+                };
+}
