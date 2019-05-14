0a0a9d13d7b96f450ac2ec8f618b844a9fd65f8a
Daniel Volinski <dan52@umbc.edu>
2019-05-14 16:21:09 -0400

    Pushing old changes from Sai

--- April17_1/mobile/src/main/java/com/example/loginpage/AccountPage.java
+++ April17_1/mobile/src/main/java/com/example/loginpage/AccountPage.java
@@ -9,17 +9,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AccountPage extends Fragment {
 
     private Button logout;
+    private TextView profile;
+    private TextView settings;
+    private TextView datausage;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        profile = (TextView) view.findViewById(R.id.profiletext);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ProfilePage.class);
+                startActivity(intent);
+            }
+        });
+
+        settings = (TextView) view.findViewById(R.id.settingstext);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SettingsPage.class);
+                startActivity(intent);
+            }
+        });
+
+        datausage = (TextView) view.findViewById(R.id.datausagetext);
+        datausage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), DatausagePage.class);
+                startActivity(intent);
+            }
+        });
+
 
         final Intent intent = new Intent(getActivity(), MainActivity.class);
         logout = (Button) view.findViewById(R.id.logoutButton);
